f7bc6ce24097e86b5175b4a19ca093ececc8e1b7
Derek <derek@findthebrink.com>
2019-11-22 02:55:14 -0800

    feat(tensorflow): base implementation complete

--- titanic__tensor-flow__base.py
+++ titanic__tensor-flow__base.py
@@ -93,8 +93,6 @@ df.drop(['Cabin', 'Name', 'Ticket', 'PassengerId'], axis=1, inplace=True)
 
 continuous = ['Age', 'Fare', 'Parch', 'Pclass', 'SibSp', 'Family_Size']
 
-# %%
-
 scaler = StandardScaler()
 
 for var in continuous:
@@ -102,3 +100,50 @@ for var in continuous:
   df[var] = scaler.fit_transform(df[var].values.reshape(-1, 1))
 
 df.head()
+
+# %%
+X_train = df[pd.notnull(df['Survived'])].drop(['Survived'], axis=1)
+y_train = df[pd.notnull(df['Survived'])]['Survived']
+X_test = df[pd.isnull(df['Survived'])].drop(['Survived'], axis=1)
+
+# %%
+import tensorflow as tf
+
+Sex = tf.feature_column.categorical_column_with_vocabulary_list('Sex', ['female', 'male'])
+Embarked = tf.feature_column.categorical_column_with_vocabulary_list('Embarked', ['S', 'C', 'Q'])
+Title = tf.feature_column.categorical_column_with_vocabulary_list('Title', ['Mr', 'Mrs', 'Miss', 'Master', 'Rev', 'Dr', 'Ms'])
+Age = tf.feature_column.numeric_column('Age')
+Fare = tf.feature_column.numeric_column('Fare')
+Parch = tf.feature_column.numeric_column('Parch')
+Pclass = tf.feature_column.numeric_column('Pclass')
+SibSp = tf.feature_column.numeric_column('SibSp')
+Family_Size = tf.feature_column.numeric_column('Family_Size')
+
+feat_cols = [Sex, Embarked, Title, Age, Fare, Parch, Pclass, SibSp, Family_Size]
+
+X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.2, random_state=1)
+
+input_func = tf.estimator.inputs.pandas_input_fn(
+  x=X_train,
+  y=y_train,
+  batch_size=100,
+  num_epochs=None,
+  shuffle=True
+)
+
+# %%
+model = tf.estimator.LinearClassifier(feature_columns=feat_cols)
+
+model.train(input_fn=input_func, max_steps=10000)
+
+pred_fn = tf.estimator.inputs.pandas_input_fn(x=X_val, batch_size=len(X_val), shuffle=False)
+predictions = list(model.predict(input_fn=pred_fn))
+
+final_preds = []
+
+# %%
+for pred in predictions:
+  final_preds.append(pred['class_ids'][0])
+
+from sklearn.metrics import classification_report
+print(classification_report(y_val, final_preds))
