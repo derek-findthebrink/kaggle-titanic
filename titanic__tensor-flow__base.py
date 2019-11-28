38bb15fcbd21decbfc6759559d8b805e802926b3
Derek <derek@findthebrink.com>
2019-11-27 17:34:13 -0800

    feat(tensorflow): complete contest submission

--- titanic__tensor-flow__base.py
+++ titanic__tensor-flow__base.py
@@ -110,22 +110,29 @@ def clean_data(dataset):
 # TODO: heatmaps for correlated features on training set
 
 
+
 # Utilities
+# ---------------------------------------------------------------------
 def find_columns_with_null(dataset):
   null_columns = dataset.columns[dataset.isnull().any()]
   return dataset[null_columns].isnull().sum()
 
 
-# %%
-# Run setup code and fetch concatenated data into var df
-setup()
-df = get_concatenated_data()
+# Complete Data
+# ---------------------------------------------------------------------
+def get_datasets():
+  setup()
+  df = get_concatenated_data()
 
-# %%
-# engineer and clean features on df
-# TODO: turn in to class that consumes data and returns instance that includes processed data
-df = engineer_features(df)
-df = clean_data(df)
+  # %%
+  # engineer and clean features on df
+  # TODO: turn in to class that consumes data and returns instance that includes processed data
+  df = engineer_features(df)
+  df = clean_data(df)
+
+  train = df[pd.notnull(df['Survived'])]
+  test = df[pd.isnull(df['Survived'])]
+  return (test, train)
 
 
 
@@ -147,84 +154,107 @@ df = clean_data(df)
 # sns.countplot(x='Title', data=df)
 
 
-# %%
-train = df[pd.notnull(df['Survived'])]
-test = df[pd.isnull(df['Survived'])]
 
 # %%
-from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split
-# from sklearn.model_selection import GridSearchCV
-# from keras.wrappers.scikit_learn import KerasClassifier
-# from keras.models import Sequential
-# from keras.layers import Dense, Activation, Dropout
+# Setup model functions
+def clean_data_for_model(df):
+  from sklearn.preprocessing import StandardScaler
 
-# from numpy.random import seed
+  dataset = df.copy().drop(['Cabin', 'Name', 'Ticket', 'PassengerId'], axis=1)
+
+  continuous = ['Age', 'Fare', 'Parch', 'Pclass', 'SibSp', 'Family_Size']
+  scaler = StandardScaler()
+  for var in continuous:
+    dataset[var] = dataset[var].astype('float64')
+    dataset[var] = scaler.fit_transform(dataset[var].values.reshape(-1, 1))
+  
+  return dataset
+
+
+def train_model(dataset):
+  from sklearn.model_selection import train_test_split
+  # from sklearn.model_selection import GridSearchCV
+  # from keras.wrappers.scikit_learn import KerasClassifier
+  # from keras.models import Sequential
+  # from keras.layers import Dense, Activation, Dropout
+
+  # from numpy.random import seed
+
+  dataset = clean_data_for_model(dataset)
+
+  X_train = dataset[pd.notnull(dataset['Survived'])].drop(['Survived'], axis=1)
+  y_train = dataset[pd.notnull(dataset['Survived'])]['Survived']
+
+  import tensorflow as tf
+
+  Sex = tf.feature_column.categorical_column_with_vocabulary_list('Sex', ['female', 'male'])
+  Embarked = tf.feature_column.categorical_column_with_vocabulary_list('Embarked', ['S', 'C', 'Q'])
+  Title = tf.feature_column.categorical_column_with_vocabulary_list('Title', ['Mr', 'Mrs', 'Master', 'Ms', 'Miss'])
+  Age = tf.feature_column.numeric_column('Age')
+  Fare = tf.feature_column.numeric_column('Fare')
+  Parch = tf.feature_column.numeric_column('Parch')
+  Pclass = tf.feature_column.numeric_column('Pclass')
+  SibSp = tf.feature_column.numeric_column('SibSp')
+  Family_Size = tf.feature_column.numeric_column('Family_Size')
+
+  feat_cols = [Sex, Embarked, Title, Age, Fare, Parch, Pclass, SibSp, Family_Size]
+
+  X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.2, random_state=1)
+
+  input_func = tf.estimator.inputs.pandas_input_fn(
+    x=X_train,
+    y=y_train,
+    batch_size=100,
+    num_epochs=None,
+    shuffle=True
+  )
+
+  model = tf.estimator.LinearClassifier(feature_columns=feat_cols)
+  model.train(input_fn=input_func, max_steps=10000)
+
+  pred_fn = tf.estimator.inputs.pandas_input_fn(x=X_val, batch_size=len(X_val), shuffle=False)
+  predictions = list(model.predict(input_fn=pred_fn))
+
+  final_predictions = []
+
+  # %%
+  for pred in predictions:
+    final_predictions.append(pred['class_ids'][0])
+
+  from sklearn.metrics import classification_report
+  print(classification_report(y_val, final_predictions))
+
+  return model
 
 # %%
-df.drop(['Cabin', 'Name', 'Ticket', 'PassengerId'], axis=1, inplace=True)
 
-continuous = ['Age', 'Fare', 'Parch', 'Pclass', 'SibSp', 'Family_Size']
+df_test, df_train = get_datasets()
+model = train_model(df_train)
 
-scaler = StandardScaler()
 
-for var in continuous:
-  df[var] = df[var].astype('float64')
-  df[var] = scaler.fit_transform(df[var].values.reshape(-1, 1))
-
-df.head()
-
-# %%
-X_train = df[pd.notnull(df['Survived'])].drop(['Survived'], axis=1)
-y_train = df[pd.notnull(df['Survived'])]['Survived']
-X_test = df[pd.isnull(df['Survived'])].drop(['Survived'], axis=1)
-
-# %%
-import tensorflow as tf
-
-Sex = tf.feature_column.categorical_column_with_vocabulary_list('Sex', ['female', 'male'])
-Embarked = tf.feature_column.categorical_column_with_vocabulary_list('Embarked', ['S', 'C', 'Q'])
-Title = tf.feature_column.categorical_column_with_vocabulary_list('Title', ['Mr', 'Mrs', 'Miss', 'Master', 'Rev', 'Dr', 'Ms'])
-Age = tf.feature_column.numeric_column('Age')
-Fare = tf.feature_column.numeric_column('Fare')
-Parch = tf.feature_column.numeric_column('Parch')
-Pclass = tf.feature_column.numeric_column('Pclass')
-SibSp = tf.feature_column.numeric_column('SibSp')
-Family_Size = tf.feature_column.numeric_column('Family_Size')
-
-feat_cols = [Sex, Embarked, Title, Age, Fare, Parch, Pclass, SibSp, Family_Size]
-
-X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.2, random_state=1)
-
-input_func = tf.estimator.inputs.pandas_input_fn(
-  x=X_train,
-  y=y_train,
-  batch_size=100,
-  num_epochs=None,
-  shuffle=True
-)
-
-# %%
-model = tf.estimator.LinearClassifier(feature_columns=feat_cols)
-
-model.train(input_fn=input_func, max_steps=10000)
-
-pred_fn = tf.estimator.inputs.pandas_input_fn(x=X_val, batch_size=len(X_val), shuffle=False)
-predictions = list(model.predict(input_fn=pred_fn))
-
-final_predictions = []
-
-# %%
-for pred in predictions:
-  final_predictions.append(pred['class_ids'][0])
-
-from sklearn.metrics import classification_report
-print(classification_report(y_val, final_predictions))
 
 # %%
 
 # final_predictions = model.predict()
 # output = pd.DataFrame({ 'PassengerId': test.PassengerId, 'Survived': final_predictions })
+df_test_clean = clean_data_for_model(df_test)
 
 # # %%
 # output.to_csv('output/tutorial__tensorflow.csv', index=False)
+
+
+# %%
+
+import tensorflow as tf
+
+pred_fn = tf.estimator.inputs.pandas_input_fn(x=df_test_clean, batch_size=len(df_test_clean), shuffle=False)
+predictions = list(model.predict(input_fn=pred_fn))
+final_predictions = []
+for pred in predictions:
+  final_predictions.append(pred['class_ids'][0])
+
+
+# %%
+test_submission = pd.DataFrame({'PassengerID': df_test.PassengerId, 'Survived': final_predictions })
+test_submission.to_csv('output/tutorial__tensorflow.csv', index=False)
+# %%
